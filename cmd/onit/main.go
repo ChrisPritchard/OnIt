0ee8363ee0dca7181d7e6d904f4bc23158d4f769
Chris <chris@grislygrotto.nz>
2026-04-06 13:20:58 +1200

    reworked to not have alarm and to display more time info

--- cmd/onit/main.go
+++ cmd/onit/main.go
@@ -22,35 +22,82 @@ func main() {
 }
 
 func render_display(ba *terminal.BufferedArea) {
-	hkt, err := time.LoadLocation("Asia/Hong_Kong")
-	if err != nil {
-		log.Fatal(err)
-	}
+	zones := get_zones()
 
-	time_display := func(time time.Time, show_colon bool) []string {
-		zone, _ := time.Zone()
-		time_string := time.Format("03:04 PM")
-		if !show_colon {
-			time_string = strings.Replace(time_string, ":", " ", 1)
-		}
+	large_display := func(time time.Time, loc *time.Location, tick bool) []string {
+		zone, time_string := get_display_time(time, loc, tick)
 		big_string := bigtext.GetBigString(time_string)
 		return append([]string{fmt.Sprintf("%s time:", zone)}, big_string...)
 	}
 
+	newline := []string{""}
 	tick := true
 	for {
-		current := time.Now()
-		message := api.GetDisplayState(current, tick)
+		now := time.Now()
+		message := api.GetDisplayState()
 
 		to_display := slices.Concat(
-			[]string{fmt.Sprintf("Epoch time: %d", current.Unix())},
-			time_display(current, tick),
-			time_display(current.In(hkt), tick),
+			newline,
+			large_display(now, zones.nzt, tick),
+			newline,
+			large_display(now, zones.hkt, tick),
+			newline,
+			[]string{fmt.Sprintf("Epoch Time: %d", now.Unix())})
+
+		var others strings.Builder
+		for _, z := range []*time.Location{zones.cet, zones.utc, zones.usp, zones.use} {
+			name, display := get_display_time(now, z, tick)
+			fmt.Fprintf(&others, "%s: %s\t", name, display)
+		}
+
+		to_display = slices.Concat(
+			to_display,
+			[]string{others.String()},
+			newline,
+			[]string{now.Format("Monday, 02 Jan 2006")},
 			message)
 
 		ba.Update(to_display)
 
 		time.Sleep(time.Second)
 		tick = !tick
+	}
+}
+
+func get_display_time(time time.Time, loc *time.Location, tick bool) (zone string, display string) {
+	rel := time.In(loc)
+	zone, _ = rel.Zone()
+	display = time.Format("03:04 PM")
+	if !tick {
+		display = strings.Replace(display, ":", " ", 1)
+	}
+	return
+}
+
+type zones struct {
+	nzt *time.Location
+	hkt *time.Location
+	cet *time.Location
+	utc *time.Location
+	usp *time.Location
+	use *time.Location
+}
+
+func get_zones() zones {
+	load_loc := func(iana string) *time.Location {
+		loc, err := time.LoadLocation(iana)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return loc
+	}
+
+	return zones{
+		nzt: load_loc("Pacific/Auckland"),
+		hkt: load_loc("Asia/Hong_Kong"),
+		cet: load_loc("Europe/Berlin"),
+		utc: load_loc("Europe/London"),
+		usp: load_loc("America/Los_Angeles"),
+		use: load_loc("America/New_York"),
 	}
 }
--- internal/api/message_api.go
+++ internal/api/message_api.go
@@ -4,27 +4,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"slices"
-	"strconv"
 	"sync"
-	"time"
 )
 
 type display_state struct {
 	mu          sync.RWMutex
-	alarmTime   *time.Time
-	alarmTitle  string
-	alarmDesc   string
-	alarmAlert  bool
 	displayText string
 }
 
 var state display_state
 
 func StartApiServer() func() {
-	http.HandleFunc("/alarm", handleAlarm)
 	http.HandleFunc("/display", handleDisplay)
-	http.HandleFunc("/clear", handleClear)
 
 	return func() {
 		fmt.Println("API server listening on :8080")
@@ -34,61 +25,15 @@ func StartApiServer() func() {
 	}
 }
 
-func GetDisplayState(currentTime time.Time, tick bool) []string {
+func GetDisplayState() []string {
 	state.mu.RLock()
 	defer state.mu.RUnlock()
 
-	message := []string{}
 	if state.displayText != "" {
-		message = []string{"", state.displayText}
+		return []string{"", state.displayText}
 	}
 
-	if state.alarmTime != nil {
-		currentUnix := currentTime.Unix()
-		alarmUnix := state.alarmTime.Unix()
-		if currentUnix < alarmUnix+60 {
-			timeSet := fmt.Sprintf("alarm set at %s:", state.alarmTime.Local().Format("03:04 PM"))
-			text := []string{timeSet, " " + state.alarmTitle, " " + state.alarmDesc}
-			if currentUnix >= alarmUnix && tick {
-				text[1] = "*\x1b[7m" + text[1]
-				text[2] = "*" + text[2] + "\x1b[0m"
-			}
-			message = slices.Concat(message, []string{""}, text)
-		}
-	}
-
-	return message
-}
-
-func handleAlarm(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query()
-
-	timeStr := query.Get("time")
-	title := query.Get("title")
-	description := query.Get("description")
-	alert := query.Get("alert") == "true"
-
-	if timeStr == "" {
-		http.Error(w, "missing time parameter", http.StatusBadRequest)
-		return
-	}
-
-	timestampSec, err := strconv.ParseInt(timeStr, 10, 64)
-	if err != nil {
-		http.Error(w, "invalid time format", http.StatusBadRequest)
-		return
-	}
-	timestamp := time.Unix(timestampSec, 0)
-
-	state.mu.Lock()
-	state.alarmTime = &timestamp
-	state.alarmTitle = title
-	state.alarmDesc = description
-	state.alarmAlert = alert
-	state.mu.Unlock()
-
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"status": "alarm set"})
+	return []string{}
 }
 
 func handleDisplay(w http.ResponseWriter, r *http.Request) {
@@ -100,17 +45,4 @@ func handleDisplay(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{"status": "display updated"})
-}
-
-func handleClear(w http.ResponseWriter, r *http.Request) {
-	state.mu.Lock()
-	state.alarmTime = nil
-	state.alarmTitle = ""
-	state.alarmDesc = ""
-	state.alarmAlert = false
-	state.displayText = ""
-	state.mu.Unlock()
-
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"status": "cleared"})
 }
