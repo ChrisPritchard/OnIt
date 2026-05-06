30b53d3b1153825962a9914e9cde0f357993265f
Chris <chris@grislygrotto.nz>
2026-05-07 11:07:51 +1200

    removed op from src

--- pi-deploy.bat
+++ pi-deploy.bat
@@ -3,5 +3,5 @@ go build -o op ./cmd/onit-disp
 go env -u GOOS GOARCH CGO_ENABLED
 ssh pizero2w "ps aux | grep ./op$ | cut -d' ' -f8 | xargs kill"
 scp .\op pizero2w:
-ssh pizero2w "chmod +x op && ./op"
+ssh pizero2w "chmod +x op && ./op" &
 del op
